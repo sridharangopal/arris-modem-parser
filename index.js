e41bc3ce61a76c3826d202a45a0386e2eb2bd29a
SRIDHARAN GOPAL <SRIDHARAN.GOPAL@LIBERTYMUTUAL.COM>
2020-10-13 16:06:38 -0400

    working copy additional changes

--- index.js
+++ index.js
@@ -1,86 +1,188 @@
 /*jshint sub:true,asi:true,maxerr:1000*/
 
-const cheerio = require('cheerio')
-const request = require('request')
-const fs = require('fs')
+import dotenv from 'dotenv'
+dotenv.config({
+    silent: true
+})
+import cheerio from 'cheerio'
+import request from 'request'
+import {
+    writeFile
+} from 'fs'
+import {
+    hostname as _hostname
+} from 'os'
 
-const url = "https://192.168.100.1/cgi-bin/status_cgi"
+import {
+    InfluxDB,
+    Point
+} from '@influxdata/influxdb-client'
+
+const {
+    load
+} = cheerio
+const {
+    get,
+    post
+} = request
+
+
+const url = process.env.url
+const token = process.env.token
+const org = process.env.org
+const bucketId = process.env.bucketId
+
+const writeApi = new InfluxDB({
+    url,
+    token
+}).getWriteApi(org, bucketId, 'ns')
+writeApi.useDefaultTags({
+    hostname: _hostname()
+})
+
+const loginUrl = "http://192.168.100.1/cgi-bin/basic_pwd_cgi"
+const auth = new Buffer.from(process.env.MODEM_USERNAME + ":" + process.env.MODEM_PASSWORD).toString("base64");
+const dataUrl = "http://192.168.100.1/cgi-bin/status_cgi"
 var msg = {}
 const tableSelector = 'table'
 const options = {
-    rowForHeadings: 0,  // extract th cells from this row for column headings (zero-based)
+    rowForHeadings: 0, // extract th cells from this row for column headings (zero-based)
     ignoreHeadingRow: true, // Don't treat the heading row as data
     ignoreRows: [],
-    downstreamChannels: 8,
-    upstreamChannels: 4
+    downstreamChannels: 32, //process.env.DOWNSTREAM_NUMBER,
+    upstreamChannels: 4 //process.env.UPSTREAM_NUMBER
 }
 const jsonReponse = []
 const columnHeadings = []
+var cred_cookie = ""
 var $ = ""
 
-request.get(url, { json: true, strictSSL: false }, (err, res, body) => {
+//login
+post(loginUrl, {
+    form: {
+        arguments: auth,
+        ar_nonce: ("" + Math.random()).substr(2, 8)
+    }
+}, (err, res, body) => {
     if (err) {
         return console.log(err);
     }
+    cred_cookie = res.headers['set-cookie'];
+    console.log(cred_cookie)
 
-    $ = cheerio.load(body)
+    // get data page
+    get(dataUrl, {
+        'headers': {
+            'Cookie': cred_cookie[0].slice(0, -1)
+        }
+    }, (err, res, body) => {
+        var points = [];
 
-    $(tableSelector).each(function (i, table) {
-        var trs = $(table).find('tr')
+        if (err) {
+            return console.log(err);
+        }
 
-        // Set up the column heading names
-        getColHeadings($(trs[options.rowForHeadings]))
+        console.log("Response body from get request : " + body)
 
-        // Process rows for data
-        $(table).find('tr').each(processRow)
-    })
+        $ = load(body)
 
-    msg.payload = {
-        // columnHeadings: columnHeadings,
-        downstream: jsonReponse.slice(0, options.downstreamChannels).map(item => {
-            const container = {};
+        $(tableSelector).each(function (i, table) {
+            var trs = $(table).find('tr')
 
-            container['DCID'] = item.DCID;
-            container['Freq'] = item.Freq;
-            container['Power'] = item.Power;
-            container['SNR'] = item.SNR;
-            container['Modulation'] = item.Modulation;
-            container['Octets'] = item.Octets;
-            container['Correcteds'] = item.Correcteds;
-            container['Uncorrectables'] = item.Uncorrectables;
+            // Set up the column heading names
+            getColHeadings($(trs[options.rowForHeadings]))
 
-            return container;
+            // Process rows for data
+            $(table).find('tr').each(processRow)
+        });
 
-        }),
-        upstream: jsonReponse.slice(options.downstreamChannels, options.downstreamChannels + options.upstreamChannels).map(item => {
-            const container = {};
+        // Writing to Influxdb Cloud
+        jsonReponse.slice(0, options.downstreamChannels).map(item => {
+            const point = new Point('downstream')
+                .tag('deviceId', 'ARRIS SBV3202')
+                .tag('DCID', item.DCID)
+                .floatField('Freq', item.Freq.split(' ')[0])
+                .floatField('Power', item.Power.split(' ')[0])
+                .floatField('SNR', item.SNR.split(' ')[0])
+                .floatField('Modulation', item.Modulation.split('Q')[0])
+                .floatField('Octets', item.Octets)
+                .floatField('Correcteds', item.Correcteds)
+                .floatField('Uncorrectables', item.Uncorrectables)
 
-            container['UCID'] = item.DCID;
-            container['Freq'] = item.Freq;
-            container['Power'] = item.Power;
-            container['ChannelType'] = item.SNR;
-            container['SymbolRate'] = item.Modulation;
-            container['Modulation'] = item.Octets;
-
-            return container;
+            points.push(point);
         })
-    }
 
-    fs.writeFile('output.json', JSON.stringify(msg, null, 4), (err) => {
-        if (err) 
-        console.log(err); 
-      else { 
-        console.log("File written successfully\n"); 
-      } 
+        try {
+            writeApi.writePoints(points)
+            // writeApi.flush()
+        } catch (err) {
+            console.log(err)
+        }
+
+
+        // Writing to filesystem
+        msg.payload = {
+            data: [
+                jsonReponse.slice(0, options.downstreamChannels).map(item => {
+                    const container = {
+                        measurement: 'downstream',
+                        tags: {
+                            deviceId: 'ARRIS SBV3202',
+                            DCID: item.DCID,
+                            location: _hostname()
+                        },
+                        fields: {}
+                    };
+
+                    container.fields['Freq'] = item.Freq.split(' ')[0];
+                    container.fields['Power'] = item.Power.split(' ')[0];
+                    container.fields['SNR'] = item.SNR.split(' ')[0];
+                    container.fields['Modulation'] = item.Modulation.split('Q')[0];
+                    container.fields['Octets'] = item.Octets;
+                    container.fields['Correcteds'] = item.Correcteds;
+                    container.fields['Uncorrectables'] = item.Uncorrectables;
+
+                    return container;
+                }),
+                jsonReponse.slice(options.downstreamChannels, options.downstreamChannels + options.upstreamChannels).map(item => {
+                    const container = {
+                        measurement: 'upstream',
+                        tags: {
+                            deviceId: 'ARRIS SBV3202',
+                            UCID: item.DCID,
+                            location: _hostname()
+                        },
+                        fields: {}
+                    };
+
+                    container.fields['Freq'] = item.Freq.split(' ')[0];
+                    container.fields['Power'] = item.Power.split(' ')[0];
+                    // container.fields['ChannelType'] = item.SNR;
+                    container.fields['SymbolRate'] = item.Modulation.split(' ')[0];
+                    container.fields['Modulation'] = item.Octets.split('Q')[0];
+
+                    return container;
+                })
+            ]
+        };
+
+        writeFile('output.json', JSON.stringify(msg.payload, null, 4), (err) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log("File written successfully\n");
+            }
+        });
+
+        return msg
     });
-
-    return msg
 });
+
 
 function getColHeadings(headingRow) {
     const alreadySeen = {}
 
-    $(headingRow).find('td').each( (j, cell) => {
+    $(headingRow).find('td').each((j, cell) => {
         let tr = $(cell).text().trim()
 
         if (alreadySeen[tr]) {
@@ -101,7 +203,7 @@ function processRow(i, row) {
     // TODO: Process options.ignoreRows
 
     $(row).find('td').each((j, cell) => {
-        let q = j+1;
+        let q = j + 1;
         rowJson[columnHeadings[q]] = $(cell).text().trim()
 
     })
